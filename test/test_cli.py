176204360b9b459ee0aa44c5e63183d859f9dc84
Michael Torres <mic.ric.tor@gmail.com>
2021-03-27 21:43:02 -0700

    Add more CLI tests

--- test/test_cli.py
+++ test/test_cli.py
@@ -1,6 +1,7 @@
 from unittest import mock
 
 from password_vault import PasswordVault, main
+from password_vault.fileformat import VaultFile
 
 
 import cmd2_ext_test
@@ -12,7 +13,7 @@ class PasswordVaultTester(cmd2_ext_test.ExternalTestMixin, PasswordVault):
         super().__init__(*args, **kwargs)
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def password_vault():
     app = PasswordVaultTester()
     app.fixture_setup()
@@ -31,6 +32,25 @@ def test_main(mock_vault, mock_exit):
     mock_vault.return_value.cmdloop.assert_called_once()
 
 
+@mock.patch("password_vault.getpass.getpass")
+def test_get_password_success(mock_getpass, password_vault):
+    mock_getpass.return_value = "hunter123"
+
+    password_vault._get_vault_password()
+
+    assert mock_getpass.call_count == 2
+
+
+@mock.patch("password_vault.getpass.getpass")
+def test_get_password_fail(mock_getpass, password_vault):
+    with pytest.raises(RuntimeError):
+        mock_getpass.side_effect = ["hunter123", "123hunter"] * 3
+
+        password_vault._get_vault_password()
+
+        assert mock_getpass.call_count == 6
+
+
 def test_cli_create(password_vault):
     password_vault._get_vault_password = mock.MagicMock()
     password_vault.app_cmd("create test_filename")
@@ -38,3 +58,19 @@ def test_cli_create(password_vault):
     password_vault._get_vault_password.assert_called_once()
     assert password_vault.vault_file_name == "test_filename"
     assert isinstance(password_vault.vault_database, dict)
+
+
+@mock.patch("password_vault.VaultFile")
+@mock.patch("password_vault.getpass.getpass")
+def test_cli_load(mock_getpass, mock_vaultfile_class, password_vault):
+    mock_vaultfile = mock.MagicMock(spec=VaultFile)
+    mock_vaultfile.read = mock.MagicMock(return_value = "{}")
+    mock_vaultfile_class.return_value = mock_vaultfile
+    mock_getpass.return_value = "test_password"
+
+    password_vault.app_cmd("load test_filename")
+    
+    mock_getpass.assert_called_once()
+    assert password_vault.vault_file_name == "test_filename"
+    mock_vaultfile_class.assert_called_once_with("test_filename", "rb+")
+    mock_vaultfile.read.assert_called_once_with(password="test_password")
